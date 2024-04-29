8a5bfd5360c273519643744b7ace5705eb41f74c
Leandro Santana <leandro7531591@gmail.com>
2024-04-28 23:10:19 -0300

    atualizando

---
+++ API_Teste_Protech/Controllers/AnimeController.cs
@@ -0,0 +1,40 @@
+﻿using ProtechAnime.Application.Services;
+using ProtechAnime.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProtechAnime.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AnimeController : ControllerBase
+    {
+        private readonly IAnimeService _animeService;
+
+        public AnimeController(IAnimeService animeService)
+        {
+            _animeService = animeService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAnimeById(int id)
+        {
+            try
+            {
+                var anime = await _animeService.GetAnimeAsync(id);
+                if (anime == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(anime);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro interno do servidor: {ex.Message}");
+            }
+        }
+    }
+}
